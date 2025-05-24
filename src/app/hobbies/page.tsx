12a8b4559c59448336f01fb69f17ccdd8ffba773
Vinicius Arcanjo <vinicius@arcanjo.dev>
2025-05-24 05:24:09 -0300

    Update heading style and remove unused code in hobbies page
    Revised the heading with improved styling for better readability and alignment. Removed commented-out code and unused imports to enhance code cleanliness and maintainability.

--- src/app/hobbies/page.tsx
+++ src/app/hobbies/page.tsx
@@ -1,6 +1,5 @@
 import { Metadata } from 'next';
 import { GamesSection } from '@/components/games-section';
-// import { GameCard } from '@/components/game-card';
 
 export const metadata: Metadata = {
   title: 'Hobbies - Vinícius Arcanjo',
@@ -10,19 +9,9 @@ export const metadata: Metadata = {
 export default function HobbiesPage() {
   return (
     <main className="space-y-12">
-      <h1 className="text-3xl font-bold">Games</h1>
+      <h1 className="text-4xl font-bold tracking-tight text-center mt-10">Games</h1>
       <section className="space-y-6">
         <GamesSection />
-        {/*<GameCard*/}
-        {/*  title="The Legend of Zelda: Breath of the Wild"*/}
-        {/*  description="Explore um vasto mundo em uma das aventuras mais épicas já criadas."*/}
-        {/*  imageUrl="https://image.api.playstation.com/vulcan/ap/rnd/202110/2000/phvVT0qZfcRms5qDAk0SI3CM.png"*/}
-        {/*  rating={4.9}*/}
-        {/*  completed={true}*/}
-        {/*  wouldPlayAgain={true}*/}
-        {/*  played={true}*/}
-        {/*/>*/}
-
       </section>
     </main>
   );
